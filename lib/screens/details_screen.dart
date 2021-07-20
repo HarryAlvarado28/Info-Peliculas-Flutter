76e0e4ab6c3778c0f15139b0ad467093ff11a987
Harry Alvarado <halvarado@bgeneral.com>
2021-07-19 23:26:02 -0500

    MovieDetails - Slivers - SliverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,11 +5,46 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-        body: Container(
-      child: Center(
-        child: Text('DetailsScreen'),
-      ),
+        body: CustomScrollView(
+      slivers: [
+        _CustomAppBar(),
+        // Container(
+        //   child: Center(
+        //     child: Text('$movie'),
+        //   ),
+        // )
+      ],
     ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+          centerTitle: true,
+          titlePadding: EdgeInsets.all(0),
+          title: Container(
+              width: double.infinity,
+              alignment: Alignment.bottomCenter,
+              color: Colors.black12,
+              child: Text('movie.title', style: TextStyle(fontSize: 16))),
+          background: FadeInImage(
+            placeholder: AssetImage('assets/loading.gif'),
+            image: NetworkImage('https://via.placeholder.com/500x300'),
+            fit: BoxFit.cover,
+          )),
+    );
   }
 }
