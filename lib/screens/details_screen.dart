c1f2109ceddabf62ed4b1dc687a49df85543511f
Harry Alvarado <halvarado@bgeneral.com>
2021-07-19 23:45:55 -0500

    Poster y título de la película

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -12,6 +12,7 @@ class DetailsScreen extends StatelessWidget {
         body: CustomScrollView(
       slivers: [
         _CustomAppBar(),
+        SliverList(delegate: SliverChildListDelegate([_PosterAndTitle()]))
         // Container(
         //   child: Center(
         //     child: Text('$movie'),
@@ -45,6 +46,55 @@ class _CustomAppBar extends StatelessWidget {
             image: NetworkImage('https://via.placeholder.com/500x300'),
             fit: BoxFit.cover,
           )),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+          SizedBox(width: 20),
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text('movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2),
+            Text('movie.originalTitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2),
+            Row(
+              children: [
+                Icon(
+                  Icons.star_outline,
+                  size: 15,
+                  color: Colors.grey,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text('movie.voteAverage', style: textTheme.caption)
+              ],
+            )
+          ])
+        ],
+      ),
     );
   }
 }
